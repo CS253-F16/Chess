ff0c3660267f8a95969e60669eb2f23949471e42
TungAnhNguyen <tnguyen2@iwu.edu>
2016-11-07 21:23:57 -0600

    Add files via upload

---
+++ static/JavaScript1.js
@@ -0,0 +1,16 @@
+﻿function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+   
+
+//WORST CASE: use JVS to invoke PHP to use SQL
